9ee3c628b4fb63d43542099036c1c3d003a6666f
huangyuchen <18210672585@163.com>
2023-02-03 16:31:30 +0800

    first commit

---
+++ components/layout.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+const name = "Nephrite";
+export const siteTitle = "Nephrite's Blog";
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Learn how to build a personal website using Next.js" />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image priority src="/images/profile.png" className={`${utilStyles.borderCircle} border-solid border-gray-500 border`} height={144} width={144} alt="" />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <Image priority src="/images/profile.png" className={utilStyles.borderCircle} height={108} width={108} alt="" />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  );
+}
